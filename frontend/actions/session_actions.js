4d1e2ae8bf9e1532c187337df409298cfb331e37
Dawinder Singh <dawindernsingh@gmail.com>
2020-03-03 10:20:26 -0800

    modidy the session actions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,4 +1,4 @@
-import * as ApiUtil from '../util/session_api_util';
+import * as APIUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
@@ -10,15 +10,32 @@ export const receiveCurrentUser = currentUser => ({
 });
 
 export const logoutCurrentUser = () => ({
-  type: LOGOUT_CURRENT_USER
+  type: LOGOUT_CURRENT_USER,
 });
 
-export const receiveSessionErrors = (errors) => ({
+export const receiveErrors = errors => ({
   type: RECEIVE_SESSION_ERRORS,
   errors
 });
 
+export const signup = user => dispatch => (
+  APIUtil.signup(user).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
 
+export const login = user => dispatch => (
+  APIUtil.login(user).then(user => (
+    dispatch(receiveCurrentUser(user))
+  ), err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
 
-
-
+export const logout = () => dispatch => (
+  APIUtil.logout().then(user => (
+    dispatch(logoutCurrentUser())
+  ))
+);
