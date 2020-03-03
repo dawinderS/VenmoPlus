7aa52ba77f84445522c94fcbbea600ca966687d2
Dawinder Singh <dawindernsingh@gmail.com>
2020-03-03 10:54:54 -0800

    'configure store and create root reducer'

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import configureStore from "./store/store";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  let store;
+  let store = configureStore();
   
   // if (window.currentUser) {
   //   const preloadedState = {
@@ -17,6 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // } else {
   //   store = configureStore();
   // }
-
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   ReactDOM.render(<h1>Welcome to Uome</h1>, root);
 });
