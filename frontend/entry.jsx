7063abd274ef7dae95dae19c090ad06f37186ca8
Dawinder Singh <dawindernsingh@gmail.com>
2020-03-03 11:03:51 -0800

    'add App and Root components'

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from "./store/store";
+import Root from "./components/root";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
@@ -18,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // } else {
   //   store = configureStore();
   // }
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  ReactDOM.render(<h1>Welcome to Uome</h1>, root);
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  ReactDOM.render(<Root store={store} />, root);
 });
